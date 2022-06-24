65e9dd14c2b75c8ab018cb38863343c8c6a2ec04
unknown <shameemkhanam@gmail.com>
2022-06-24 23:46:56 +0530

    second commit

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ let resultType = document.getElementById("resultType");
 let inputTypeValue, resultTypeValue;
 
 input.addEventListener("input", myResult);
+
 result.addEventListener("input", myResult1);
 
+inputType.addEventListener("change", myResult1);
 
-inputType.addEventListener("change", myResult);
-resultType.addEventListener("change", myResult1);
+resultType.addEventListener("change", myResult);
 
 inputTypeValue = inputType.value;
 resultTypeValue = resultType.value;
@@ -20,27 +21,27 @@ function myResult() {
   inputTypeValue = inputType.value;
   resultTypeValue = resultType.value;
 
-  if( inputTypeValue === "meter" && resultTypeValue == "kilometer"){
+  if( inputTypeValue === "meter" && resultTypeValue === "kilometer"){
     result.value = Number(input.value / 1000) ;
-  }else if (inputTypeValue === "meter" && resultTypeValue == "centimeter"){
+  }else if (inputTypeValue === "meter" && resultTypeValue === "centimeter"){
     result.value = Number(input.value * 100) ;
-  }else if(inputTypeValue === "meter" && resultTypeValue == "meter"){
+  }else if(inputTypeValue === "meter" && resultTypeValue === "meter"){
     result.value = input.value ;
   }
 
-  if( inputTypeValue === "kilometer" && resultTypeValue == "kilometer"){
+  if( inputTypeValue === "kilometer" && resultTypeValue === "kilometer"){
     result.value = input.value ;
-  }else if (inputTypeValue === "kilometer" && resultTypeValue == "centimeter"){
+  }else if (inputTypeValue === "kilometer" && resultTypeValue === "centimeter"){
     result.value = Number(input.value * 100000) ;
-  }else if(inputTypeValue === "kilometer" && resultTypeValue == "meter"){
+  }else if(inputTypeValue === "kilometer" && resultTypeValue === "meter"){
     result.value = Number(input.value * 1000) ;
   }
 
-  if( inputTypeValue === "centimeter" && resultTypeValue == "kilometer"){
+  if( inputTypeValue === "centimeter" && resultTypeValue === "kilometer"){
     result.value = Number(input.value / 100000) ;
-  }else if (inputTypeValue === "centimeter" && resultTypeValue == "centimeter"){
+  }else if (inputTypeValue === "centimeter" && resultTypeValue === "centimeter"){
     result.value = input.value ;
-  }else if(inputTypeValue === "centimeter" && resultTypeValue == "meter"){
+  }else if(inputTypeValue === "centimeter" && resultTypeValue === "meter"){
     result.value = Number(input.value / 100) ;
   }
 
@@ -55,27 +56,27 @@ function myResult1(){
   inputTypeValue = inputType.value;
   resultTypeValue = resultType.value;
 
-  if( resultTypeValue == "kilometer" && inputTypeValue === "meter"){
+  if( resultTypeValue === "kilometer" && inputTypeValue === "meter"){
     input.value = Number(result.value * 1000) ;
   }else if (resultTypeValue == "centimeter" && inputTypeValue === "meter"  ){
     input.value = Number(result.value / 100) ;
-  }else if(resultTypeValue == "meter" && inputTypeValue === "meter" ){
+  }else if(resultTypeValue === "meter" && inputTypeValue === "meter" ){
     input.value = result.value ;
   }
 
-  if( resultTypeValue == "kilometer" && inputTypeValue === "kilometer" ){
+  if( resultTypeValue === "kilometer" && inputTypeValue === "kilometer" ){
     input.value = result.value ;
-  }else if ( resultTypeValue == "centimeter" && inputTypeValue === "kilometer"  ){
+  }else if ( resultTypeValue === "centimeter" && inputTypeValue === "kilometer"  ){
     input.value = Number(result.value / 100000) ;
-  }else if( resultTypeValue == "meter" && inputTypeValue === "kilometer"  ){
+  }else if( resultTypeValue === "meter" && inputTypeValue === "kilometer"  ){
     input.value = Number(result.value / 1000) ;
   }
 
-  if( resultTypeValue == "kilometer" && inputTypeValue === "centimeter"  ){
+  if( resultTypeValue === "kilometer" && inputTypeValue === "centimeter"  ){
     input.value = Number(result.value * 100000) ;
-  }else if ( resultTypeValue == "centimeter" && inputTypeValue === "centimeter"  ){
+  }else if ( resultTypeValue === "centimeter" && inputTypeValue === "centimeter"  ){
     input.value = result.value ;
-  }else if( resultTypeValue == "meter" && inputTypeValue === "centimeter"  ){
+  }else if( resultTypeValue === "meter" && inputTypeValue === "centimeter"  ){
     input.value = Number(result.value * 100) ;
   }
 
